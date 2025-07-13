45b00d3dbd915568303c50830586a9f0f5f7e818
Fuad52 <fatinfuad25@gmail.com>
2025-07-14 04:16:09 +0600

    second commit

---
+++ _11_DataTypeVariable.java
@@ -0,0 +1,30 @@
+public class _11_DataTypeVariable {
+    public static void main(String[] args){
+        
+        boolean b = true;
+
+        char c = 'A';
+
+        byte by = 100;
+
+        short s = 10000;
+
+        int i = 100000;
+
+        long l = 10000000000L;
+
+        float f = 10.5f;
+
+        double d = 20.99;
+
+        System.out.println("Boolean value: " + b);
+        System.out.println("Character value: " + c);
+        System.out.println("Byte value: " + by);
+        System.out.println("Short value: " + s);
+        System.out.println("Integer value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+    }
+    
+}
